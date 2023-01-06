82f26a5d7ebefd6bc7d2352c3cca687acb9d920c
user202729 <25191436+user202729@users.noreply.github.com>
2023-01-06 10:54:14 +0700

    Move TeX helper files here

--- test/test_main.py
+++ test/test_main.py
@@ -17,7 +17,7 @@ from pathlib import Path
 for name in ["test_pythonimmediate.tex", "test_pythonimmediate_file.py"]:
 	a=Path("/tmp")/name
 	a.unlink(missing_ok=True)
-	a.symlink_to(Path(__file__).parent.parent.parent/name)
+	a.symlink_to(Path(__file__).parent.parent/"tex"/name)
 
 
 class Test:
