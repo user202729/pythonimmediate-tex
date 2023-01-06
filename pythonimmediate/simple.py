92bdd8e8af7c2fbc184100f5447a23c8ccc5a0a1
user202729 <25191436+user202729@users.noreply.github.com>
2023-01-06 09:02:44 +0700

    Write more documentation

--- pythonimmediate/simple.py
+++ pythonimmediate/simple.py
@@ -1,5 +1,7 @@
 r"""
 Simple interface, suitable for users who may not be aware of TeX subtleties, such as category codes.
+
+Start with reading  :func:`newcommand` and :func:`execute`.
 """
 
 import sys
@@ -77,8 +79,55 @@ def replace_double_hash(s: str)->str:
 @export_function_to_module
 @user_documentation
 def get_arg_str(engine: Engine=  default_engine)->str:
-	"""
-	Get a mandatory argument.
+	r"""
+	Get a mandatory argument from the input stream.
+
+	It's slightly difficult to explain what this does, so here's an example::
+
+		@newcommand
+		def foo():
+			a = get_arg_str()
+			b = get_arg_str()
+			c = get_arg_str()
+
+	The above defines a [TeX] function ``\foo``, if it's called in [TeX] code as::
+
+		\foo{xx}{yy}{zz}
+
+	then the variable ``a`` will be the string ``"xx"``, ``b`` will be ``"yy"``, and ``c`` will be ``"zz"``.
+
+	.. _str-tokenization:
+
+	Note on parameter tokenization
+	------------------------------
+
+	.. note::
+		This function, as well as all the `_str` functions, return the argument that might be mangled in the following ways:
+
+		- A space might be added after each command that consist of multiple characters
+		- the ``^^xx`` or ``^^xxxx`` etc. notation is replaced by the character itself
+		- multiple space characters may be collapsed into one
+		- newline character may become space character
+		- double-newline character may become ``\par``
+
+		For example, ``\*\hell^^6F{  }`` may become the string ``r"\*\hello { }"`` in Python.
+
+		As such, verbatim-like commands in the argument are not supported. See :func:`get_verb_arg` for a workaround.
+
+		Nevertheless, if the argument consist of only "normal" [TeX] commands, re-executing the string should do the same
+		thing as the original code.
+
+		In case of doubt, print out the string and check it manually.
+
+	.. note::
+
+		For advanced users:
+
+		It gets the argument, detokenize it, pass it through :func:`replace_double_hash`, and return the result.
+
+		This is the simple API, as such it assumes normal category code values.
+		Refer to :meth:`BalancedTokenList.get_next()` for a more advanced API.
+
 	"""
 	return replace_double_hash(typing.cast(Callable[[Engine], TTPEmbeddedLine], Python_call_TeX_local(
 		r"""
@@ -93,6 +142,19 @@ def get_arg_str(engine: Engine=  default_engine)->str:
 @export_function_to_module
 @user_documentation
 def get_arg_estr(engine: Engine=  default_engine)->str:
+	r"""
+	Get a mandatory argument from the input stream, then process it as described in :ref:`estr-expansion`.
+
+	.. _estr-expansion:
+	Note on argument expansion of ``estr``-type functions
+	-----------------------------------------------------
+
+	These functions return a string, however they're meant for strings other than [TeX] code.
+
+	The argument is fully expanded, and any "escaped character" are passed as the character itself to Python.
+
+	The behavior is similar to that of the ``\py`` command argument processing, refer to the [TeX] package documentation.
+	"""
 	return str(typing.cast(Callable[[Engine], Tuple[TTPEBlock]], Python_call_TeX_local(
 r"""
 
@@ -108,22 +170,22 @@ r"""
 @user_documentation
 def get_optional_arg_str(engine: Engine=  default_engine)->Optional[str]:
 	"""
-	Get an optional argument.
+	Get an optional argument. See also :ref:`str-tokenization`.
 	"""
 	[result]=typing.cast(Callable[[Engine], Tuple[TTPLine]], Python_call_TeX_local(
-r"""
-\NewDocumentCommand %name% {o} {
-	\immediate\write \__write_file {
-		r ^^J
-		\IfNoValueTF {#1} {
-			0
-		} {
-			\unexpanded{1 #1}
+		r"""
+		\NewDocumentCommand %name% {o} {
+			\immediate\write \__write_file {
+				r ^^J
+				\IfNoValueTF {#1} {
+					0
+				} {
+					\unexpanded{1 #1}
+				}
+			}
+			\__read_do_one_command:
 		}
-	}
-	\__read_do_one_command:
-}
-""", recursive=False))(engine)
+		""", recursive=False))(engine)
 	result_=str(result)
 	if result_=="0": return None
 	assert result_[0]=="1", result_
@@ -134,18 +196,21 @@ r"""
 @export_function_to_module
 @user_documentation
 def get_optional_arg_estr(engine: Engine=  default_engine)->Optional[str]:
+	"""
+	Get an optional argument. See also :ref:`estr-expansion`.
+	"""
 	[result]=typing.cast(Callable[[Engine], Tuple[TTPEBlock]], Python_call_TeX_local(
-r"""
-\NewDocumentCommand %name% {o} {
-	%sync%
-	\IfNoValueTF {#1} {
-		%send_arg0(0)%
-	} {
-		%send_arg0(1 #1)%
-	}
-	\__read_do_one_command:
-}
-""", recursive=False)) (engine)
+		r"""
+		\NewDocumentCommand %name% {o} {
+			%sync%
+			\IfNoValueTF {#1} {
+				%send_arg0(0)%
+			} {
+				%send_arg0(1 #1)%
+			}
+			\__read_do_one_command:
+		}
+		""", recursive=False))(engine)
 	result_=str(result)
 	if result_=="0": return None
 	assert result_[0]=="1", result_
@@ -155,27 +220,46 @@ r"""
 @export_function_to_module
 @user_documentation
 def get_verb_arg(engine: Engine=  default_engine)->str:
-	"""
-	Get a verbatim argument. Since it's verbatim, there's no worry of |#| being doubled,
-	but it can only be used at top level.
+	r"""
+	Get a verbatim argument.
+
+	Similar to ``\verb``, defined [TeX] commands that use this function
+	can only be used at top level i.e. not inside any arguments.
+
+	This function behavior is identical to ``v``-type argument in ``xparse`` package, you can use it like this::
+
+		\foo{xx%^\?}  % delimited with braces
+		\foo|xx%^\?|  % delimited with another symbol
+
+	.. seealso::
+
+		:func:`get_multiline_verb_arg` to support newline character in the argument.
+
+	.. note::
+
+		Hard TAB character in the argument gives an error until the corresponding LaTeX3 bug is fixed,
+		see https://tex.stackexchange.com/q/508001/250119.
 	"""
 	return str(typing.cast(Callable[[Engine], Tuple[TTPLine]], Python_call_TeX_local(
-r"""
-\NewDocumentCommand %name% {v} {
-	\immediate\write\__write_file { \unexpanded {
-		r ^^J
-		#1
-	}}
-	\__read_do_one_command:
-}
-""", recursive=False))
-		(engine)[0])
+		r"""
+		\NewDocumentCommand %name% {v} {
+			\immediate\write\__write_file { \unexpanded {
+				r ^^J
+				#1
+			}}
+			\__read_do_one_command:
+		}
+		""", recursive=False))(engine)[0])
 
 @export_function_to_module
 @user_documentation
 def get_multiline_verb_arg(engine: Engine=  default_engine)->str:
-	"""
-	Get a multi-line verbatim argument.
+	r"""
+	Get a multi-line verbatim argument. Usage is identical to :func:`get_verb_arg`, except newline characters
+	in the argument is supported.
+
+	.. note::
+		in unusual category regime (such as that in ``\ExplSyntaxOn``), it may return wrong result.
 	"""
 	return str(typing.cast(Callable[[Engine], Tuple[TTPBlock]], Python_call_TeX_local(
 r"""
@@ -195,25 +279,25 @@ def check_function_name(name: str)->None:
 	if not re.fullmatch("[A-Za-z]+", name) or (len(name)==1 and ord(name)<=0x7f):
 		raise RuntimeError("Invalid function name: "+name)
 
-def newcommand_(name: str, f: Callable, engine: Engine)->Callable:
+def _newcommand(name: str, f: Callable, engine: Engine)->Callable:
 	identifier=get_random_identifier()
 
 	typing.cast(Callable[[PTTVerbatimLine, PTTVerbatimLine, Engine], None], Python_call_TeX_local(
-r"""
-\cs_new_protected:Npn %name% {
-	\begingroup
-		\endlinechar=-1~
-		%read_arg0(\__line)%
-		%read_arg1(\__identifier)%
-		\cs_new_protected:cpx {\__line} {
-			\unexpanded{\immediate\write \__write_file} { i \__identifier }
-			\unexpanded{\__read_do_one_command:}
+		r"""
+		\cs_new_protected:Npn %name% {
+			\begingroup
+				\endlinechar=-1~
+				%read_arg0(\__line)%
+				%read_arg1(\__identifier)%
+				\cs_new_protected:cpx {\__line} {
+					\unexpanded{\immediate\write \__write_file} { i \__identifier }
+					\unexpanded{\__read_do_one_command:}
+				}
+			\endgroup
+			%optional_sync%
+			\__read_do_one_command:
 		}
-	\endgroup
-	%optional_sync%
-	\__read_do_one_command:
-}
-""", recursive=False)) (PTTVerbatimLine(name), PTTVerbatimLine(identifier), engine)
+		""", recursive=False))(PTTVerbatimLine(name), PTTVerbatimLine(identifier), engine)
 
 	_code=define_TeX_call_Python(
 			lambda engine: run_code_redirect_print_TeX(f, engine=engine),
@@ -225,22 +309,22 @@ def renewcommand_(name: str, f: Callable, engine: Engine)->Callable:
 	identifier=get_random_identifier()
 
 	typing.cast(Callable[[PTTVerbatimLine, PTTVerbatimLine, Engine], None], Python_call_TeX_local(
-r"""
-\cs_new_protected:Npn %name% {
-	\begingroup
-		\endlinechar=-1~
-		\readline \__read_file to \__line
-		\readline \__read_file to \__identifier
-		\exp_args:Ncx \renewcommand {\__line} {
-			\unexpanded{\immediate\write \__write_file} { i \__identifier }
-			\unexpanded{\__read_do_one_command:}
+		r"""
+		\cs_new_protected:Npn %name% {
+			\begingroup
+				\endlinechar=-1~
+				\readline \__read_file to \__line
+				\readline \__read_file to \__identifier
+				\exp_args:Ncx \renewcommand {\__line} {
+					\unexpanded{\immediate\write \__write_file} { i \__identifier }
+					\unexpanded{\__read_do_one_command:}
+				}
+				\exp_args:Nc \MakeRobust {\__line}  % also make the command global
+			\endgroup
+			%optional_sync%
+			\__read_do_one_command:
 		}
-		\exp_args:Nc \MakeRobust {\__line}  % also make the command global
-	\endgroup
-	%optional_sync%
-	\__read_do_one_command:
-}
-""", recursive=False)) (PTTVerbatimLine(name), PTTVerbatimLine(identifier), engine)
+		""", recursive=False))(PTTVerbatimLine(name), PTTVerbatimLine(identifier), engine)
 	# TODO remove the redundant entry from TeX_handlers (although technically is not very necessary, just cause slight memory leak)
 	#try: del TeX_handlers["u"+name]
 	#except KeyError: pass
@@ -255,18 +339,78 @@ r"""
 def newcommand(x: Union[str, Callable, None]=None, f: Optional[Callable]=None, engine: Engine=  default_engine)->Callable:
 	r"""
 	Define a new [TeX]-command.
-	If name is not provided, it's automatically deduced from the function.
+
+	Example::
+
+		@newcommand
+		def myfunction():
+			print_TeX("Hello world!", end="")
+	
+	The above is mostly equivalent to ``\newcommand{\myfunction}{Hello world!}``,
+	it defines a [TeX] command ``\myfunction`` that prints ``Hello world!``.
+
+	See also documentation of :func:`print_TeX` to understand the example above.
+
+	It can be used as either a decorator or a function::
+
+		def myfunction():
+			print_TeX("Hello world!", end="")
+
+		newcommand("myfunction", myfunction)
+
+	An explicit command name can also be provided::
+
+		@newcommand("hello")
+		def myfunction():
+			print_TeX("Hello world!", end="")
+
+	The above defines a [TeX] command ``\hello`` that prints ``Hello world!``.
+
+	If name is not provided, it's automatically deduced from the Python function name.
+
+	The above is not by itself very useful. Read the documentation of :func:`get_arg_str` for a guide on how to define commands that take arguments.
+
+	.. _trailing-newline:
+
+	Note on trailing newline
+	------------------------
+
+	.. note::
+
+		Regarding the ``end=""`` part in the example above, it's used to prevent a spurious space, otherwise if you run the [TeX] code::
+
+			123\myfunction 456
+		
+		it will be "equivalent" to::
+
+			123Hello world!
+			456
+
+		and the newline inserts a space.
+
+		Internally, the feature is implemented by appending ``%`` to the last line, as most of the time the code::
+
+			123Hello world!%
+			456
+
+		will be equivalent to::
+
+			123Hello world!456
+
+		but in unusual category code cases or cases of malformed [TeX] code (such as a trailing backslash at the end of the line),
+		it may not be equivalent. Use the advanced API, or always include a final newline and manually add the comment character,
+		if these cases happen.
+
 	"""
 	if f is not None: return newcommand(x, engine=engine)(f)
-	if x is None: return newcommand  # weird design but okay (allow |@newcommand()| as well as |@newcommand|)
-	if isinstance(x, str): return functools.partial(newcommand_, x, engine=engine)
-	return newcommand_(x.__name__, x, engine=engine)
+	if x is None: return functools.partial(newcommand, engine=engine)
+	if isinstance(x, str): return functools.partial(_newcommand, x, engine=engine)
+	return _newcommand(x.__name__, x, engine=engine)
 
 @export_function_to_module
 def renewcommand(x: Union[str, Callable, None]=None, f: Optional[Callable]=None, engine: Engine=  default_engine)->Callable:
 	r"""
-	Redefine a [TeX]-command.
-	If name is not provided, it's automatically deduced from the function.
+	Redefine a [TeX]-command. Usage is similar to :func:`newcommand`.
 	"""
 	if f is not None: return renewcommand(x, engine=engine)(f)
 	if x is None: return renewcommand  # weird design but okay (allow |@newcommand()| as well as |@newcommand|)
@@ -275,21 +419,62 @@ def renewcommand(x: Union[str, Callable, None]=None, f: Optional[Callable]=None,
 
 @user_documentation
 @export_function_to_module
-def execute(block: str, engine: Engine)->None:
+def execute(block: str, engine: Engine=default_engine)->None:
 	r"""
 	Run a block of [TeX]-code (might consist of multiple lines).
-	Catcode-changing commands are allowed inside.
 
-	A simple example is |pythonimmediate.run_block_local('123')| which simply typesets |123|.
+	A simple example is ``execute('123')`` which simply typesets ``123`` (with a trailing newline, see the note in :func:`newcommand` documentation).
 
-	A more complicated example is |pythonimmediate.run_block_local(r'\verb+%+')|.
+	This is similar to :func:`print_TeX` but it's executed immediately, and any error is immediately reported
+	with Python traceback points to the caller.
+
+	On the other hand, because this is executed immediately, each part must be "self-contained".
+	With :func:`print_TeX` you can do the following::
+
+		print_TeX(r"\abc", end="")
+		print_TeX(r"def", end="")
+
+	and a single command ``\abcdef`` will be executed, but it does not work with this method.
+
+	As another consequence, all :func:`execute` are done before all :func:`print_TeX` in the same block.
+	For example::
+
+		print_TeX(r"3")
+		execute(r"1")
+		print_TeX(r"4")
+		execute(r"2")
+
+	will typeset ``1 2 3 4``.
+
+	.. note::
+
+		For advanced users: it's implemented with ``\scantokens``, so catcode-changing commands are allowed inside.
 	"""
 	run_block_local(block, engine=engine)
 
 @export_function_to_module
 def print_TeX(*args, **kwargs)->None:
+	r"""
+	Unlike other packages, the normal ``print()`` function prints to the console.
+
+	This function can be used to print [TeX] code to be executed. For example::
+
+		\begin{pycode}
+		print_TeX("Hello world!")
+		\end{pycode}
+
+	It can also be used inside a custom-defined command, see the documentation of :func:`newcommand` for an example.
+
+	The signature is identical to that of ``print()``. Passing explicit ``file=`` argument is not allowed.
+
+	Remark: normally the ``print()`` function prints out a newline with each invocation.
+	A newline will appear as a spurious space in the output
+	Use ``print_TeX(end="")`` to prevent this, see :ref:`trailing-newline`.
+	"""
 	if not hasattr(pythonimmediate, "file"):
 		raise RuntimeError("Internal error: attempt to print to TeX outside any environment!")
+	if "file" in kwargs:
+		raise TypeError("print_TeX() got an unexpected keyword argument 'file'")
 	if pythonimmediate.file is not None:
 		functools.partial(print, file=pythonimmediate.file)(*args, **kwargs)  # allow user to override `file` kwarg
 
